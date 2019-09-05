e25340718d24cd91f6e594e39df24cb27a1f7ab7
Ilya <i1ya1l@yandex.ru>
2019-09-06 01:22:51 +0300

    init

---
+++ src/component/Self/Self.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import _one from './images/1.png';
+import './style.scss';
+import { Route, Link, NavLink } from "react-router-dom";
+
+const Self = () => {
+  return (
+    <div className="self">
+      <div className="self__header">
+        <h1 className="self__title">Компоненты</h1>
+      </div>
+      <div className="self__content">
+        <div className="self__list selfList">
+          <NavLink to="/DataTable" className="self__item selfItem">
+            <div className="selfItem__contains">
+              <figure className="selfItem__figure">
+                <img className="selfItem__image" src={_one} alt="Submit" />
+              </figure>
+              <div className="selfItem__content">
+                <span className="selfItem__name">
+                  React-приложение для отображения таблицы с данными.
+                </span>
+                <div className="selfItem__desc">
+                  <p className="selfItem__descCaption">
+                    Функционал
+                  </p>
+                  <ul className="selfItem__list list">
+                    <li className="list__item">Сортировка по столбцам.</li>
+                    <li className="list__item">Клиентская пагинация</li>
+                    <li className="list__item">Фильтрация</li>
+                    <li className="list__item">По клике на строку таблицы значения полей выводятся в дополнительном блоке.</li>
+                    <li className="list__item">Кнопка добавить, по нажатии на которую выпадает форма добавления ряда</li>
+                    <li className="list__item">После заполнения всех инпутов активируется кнопка Добавить в таблицу которая вставляет заполненный ряд в начало таблицы</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </NavLink>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Self;
